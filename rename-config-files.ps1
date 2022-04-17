a4944d9f3d08105810ed0747183ebf21678255d1
Dale Koch <dalek@marvin.com>
2022-04-17 13:57:34 -0500

    script fix

--- rename-config-files.ps1
+++ rename-config-files.ps1
@@ -2,6 +2,6 @@ $files = Get-ChildItem -File -Filter *.cfg -Path retroarch\config\Mupen64Plus-Ne
 
 for ($i=0; $i -lt $files.Count; $i++) {
     Write-Output $files[$i].FullName
-    $new_name = $files[$i].BaseName + ".chd" + $files[$i].Extension
+    $new_name = $files[$i].BaseName + ".zip" + $files[$i].Extension
     Rename-Item -Path $files[$i].FullName -NewName $new_name
 }
